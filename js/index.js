400cd666df6f622b5810e092213f658bda96b088
saranpradeepj <saranpradeep2003@gmail.com>
2024-01-18 14:29:57 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+var acc = document.getElementsByClassName("fourthSection-accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    //this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var mySwiper = new Swiper(".swiper-container", {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "1",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
